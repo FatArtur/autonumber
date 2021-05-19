9abf8b4b7f1214c735e621697fe6f47eb493af69
AR2R <mega_4tya@bk.ru>
2021-05-19 23:53:49 +0300

    fix and add

--- src/main/java/com/example/autonumber/controller/NumberController.java
+++ src/main/java/com/example/autonumber/controller/NumberController.java
@@ -1,7 +1,7 @@
 package com.example.autonumber.controller;
 
 
-import com.example.autonumber.model.AutoNumber;
+import com.example.autonumber.sevice.NumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("number")
 public class NumberController {
-    private AutoNumber autoNumber;
+    private NumberService numberService;
 
     @Autowired
-    public NumberController(AutoNumber autoNumber) {
-        this.autoNumber = autoNumber;
+    public NumberController(NumberService numberService) {
+        this.numberService = numberService;
     }
 
     @GetMapping("random")
-    public String random (){
-        return autoNumber.getRandomNumber();
+    public String random() {
+        return numberService.getRandomNumber();
     }
 
     @GetMapping("next")
-    public String next (){
-        return autoNumber.getNextNumber();
+    public String next() {
+        return numberService.getNextNumber();
     }
 }
--- src/main/java/com/example/autonumber/model/AutoNumber.java
+++ src/main/java/com/example/autonumber/model/AutoNumber.java
@@ -3,46 +3,25 @@ package com.example.autonumber.model;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
-
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @Data
 public class AutoNumber {
-    private String text;
-    private int number;
+    private volatile String text;
+    public AtomicInteger number = new AtomicInteger();
     private final String REGION = " 116 RUS";
     private final List<Character> boxLetter = new ArrayList<>(Arrays
             .asList('А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'));
-    private Random random = new Random();
-    private static List<String> boxNumbers;
-
-    static {
-        boxNumbers = new ArrayList<>();
-    }
-
-    public String getRandomNumber() {
-        randomValue();
-        while (repeatAutoNumber(text, number)) {
-            randomValue();
-        }
-        boxNumbers.add(text + number);
-        return generatedAutoNumber(text, number);
-    }
-
-    private void randomValue() {
-        number = random.nextInt(999) + 1;
-        text = "" + boxLetter.get(random.nextInt(12)) + boxLetter.get(random.nextInt(12))
-                + boxLetter.get(random.nextInt(12));
-    }
 
 
-    private String generatedAutoNumber(String text, int number) {
+    public String generatedAutoNumber(String text, int number) {
         String result = text.substring(0, 1) + convertNumberToString(number) + text.substring(1, 3) + REGION;
         return result;
     }
 
-    private String convertNumberToString(int number) {
+    protected String convertNumberToString(int number) {
         String result;
         if (number < 10) {
             result = "00" + number;
@@ -50,55 +29,5 @@ public class AutoNumber {
             result = "0" + number;
         } else result = "" + number;
         return result;
-    }
-
-    private boolean repeatAutoNumber(String text, int val) {
-        return boxNumbers.stream().anyMatch(s -> s.equals(text + val));
-    }
-
-    public String getNextNumber() {
-        if (text == null) {
-            text = "ААА";
-            number++;
-        }
-        char[] letters = text.toCharArray();
-        if (number < 999) {
-            boxNumbers.add(text + number);
-            return generatedAutoNumber(text, ++number);
-        }
-        if (number == 999) {
-            number = 001;
-            if (letters[2] != 'Х') {
-                text = "" + letters[0] + letters[1] + boxLetter.get(boxLetter.indexOf(letters[2]) + 1);
-                boxNumbers.add(text + number);
-                return generatedAutoNumber(text, number);
-            }
-            if (letters[2] == 'Х') {
-                if (letters[1] != 'Х') {
-                    text = "" + letters[0] + boxLetter.get(boxLetter.indexOf(letters[1]) + 1)
-                            + boxLetter.get(0);
-                    boxNumbers.add(text + number);
-                    return generatedAutoNumber(text, number);
-                }
-                if (letters[1] == 'Х') {
-                    if (letters[0] != 'Х') {
-                        text = "" + boxLetter.get(boxLetter.indexOf(letters[1]) + 1)
-                                + boxLetter.get(0)
-                                + boxLetter.get(0);
-                        boxNumbers.add(text + number);
-                        return generatedAutoNumber(text, number);
-                    }
-                    if (letters[0] == 'Х') {
-                        text = "" + boxLetter.get(0)
-                                + boxLetter.get(0)
-                                + boxLetter.get(0);
-                        boxNumbers.add(text + number);
-                        return generatedAutoNumber(text, number);
-                    }
-                }
-            }
-        }
-        boxNumbers.add(text + number);
-        return generatedAutoNumber(text, number);
     }
 }
